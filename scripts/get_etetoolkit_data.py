4550c5021d2678c773c3988782cae57745ea82ef
nylander <Johan.Nylander@nrm.se>
2026-04-15 13:44:00 +0200

    Remove print statement

--- scripts/get_etetoolkit_data.py
+++ scripts/get_etetoolkit_data.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 import sys
-import os 
+import os
 
 with open(sys.argv[1],'r') as f:
     using_conda = f.read().strip()
 
 if using_conda == "True":
-    print("Installing ete3 NCBI data")
+    #print("Installing ete3 NCBI data")
     from ete3 import NCBITaxa
     ncbi = NCBITaxa()
     os.system("echo 'data added' > {}".format(sys.argv[2]))
